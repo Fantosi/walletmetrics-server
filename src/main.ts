7e0c01c1f140503b609e96e13086afe2f2a1f6fb
jewelrykim <jsjs21good@gmail.com>
2023-06-03 18:33:56 +0900

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: "*",
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+      credentials: true,
+    },
+  });
   await app.listen(3000);
 }
 bootstrap();
