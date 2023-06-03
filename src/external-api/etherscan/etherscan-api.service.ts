b87e817f7594ce16aaea2419f68d97cb6758552f
everGreenGH <skin0011@naver.com>
2023-06-03 15:54:30 +0900

    fix: etherscan api service refactor

--- src/external-api/etherscan/etherscan-api.service.ts
+++ src/external-api/etherscan/etherscan-api.service.ts
@@ -32,42 +32,9 @@ export class EtherscanApiService {
     check();
   }
 
-  async getTxList() {
-    const result: TransactionDto[] = [];
-    // const baseUrl = this.configService.get<string>("EtherscanApi.url");
-    // const apiKey = this.configService.get<string>("EtherscanApi.apiKey");
-
-    const request: EtherScanTxListRequest = {
-      address: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
-      startblock: "0",
-      endblock: "99999999",
-      page: "1",
-      offset: "10",
-      sort: "desc",
-      apikey: this.apiKey,
-    };
-
-    const url = `${this.baseUrl}&address=${request.address}&startblock=${request.startblock}&endblock=${request.endblock}&page=${request.page}&offset=${request.offset}&sort=${request.sort}&apikey=${request.apikey}`;
-
-    const { data } = await firstValueFrom(this.httpService.get<EtherScanResponse>(url).pipe());
-
-    if (data.status === "1") {
-      data.result.forEach((tx) => {
-        result.push(tx);
-      });
-    }
-
-    for (const tx of result) {
-      await this.createTransaction(tx);
-    }
-    // await this.deleteTables();
-    return result;
-  }
-
-  async createTransaction(transactionDto: TransactionDto): Promise<boolean> {
+  private async _createTransaction(transactionDto: TransactionDto): Promise<boolean> {
     const { from: walletAddress, to: protocolAddress, timeStamp, value, hash, blockNumber } = transactionDto;
     try {
-      console.log("createTransaction", JSON.stringify(transactionDto));
       let wallet = await this.walletRepository.findOne({ where: { walletAddress } });
       if (wallet == null) {
         wallet = await this.walletRepository.create({
@@ -78,7 +45,6 @@ export class EtherscanApiService {
       }
       wallet = await this.walletRepository.save(wallet);
       let protocol = await this.protocolRepository.findOne({ where: { protocolAddress } });
-      console.log(protocol);
 
       if (protocol == null) {
         protocol = await this.protocolRepository.create({
@@ -92,7 +58,7 @@ export class EtherscanApiService {
       protocol = await this.protocolRepository.save(protocol);
 
       const transaction = await this.transactionRepository.findOne({ where: { hash } });
-      console.log("------", transaction);
+
       if (transaction == null) {
         await this.transactionRepository.save({
           wallet,
@@ -116,19 +82,7 @@ export class EtherscanApiService {
     }
   }
 
-  async deleteTables() {
-    try {
-      await this.transactionRepository.delete({});
-      await this.walletRepository.delete({});
-      await this.protocolRepository.delete({});
-      return true;
-    } catch (error) {
-      console.error(error);
-      return false;
-    }
-  }
-
-  async checkIsSyncedByBlockNumber(protocolAddress: string): Promise<boolean> {
+  private async _checkIsSyncedByBlockNumber(protocolAddress: string): Promise<boolean> {
     try {
       const protocol = await this.protocolRepository.findOne({ where: { protocolAddress } });
 
@@ -189,7 +143,7 @@ export class EtherscanApiService {
   async syncTransactions(protocolAddress: string): Promise<boolean> {
     try {
       const lowerCasedProtocolAddress = protocolAddress.toLowerCase();
-      const isSynced = await this.checkIsSyncedByBlockNumber(lowerCasedProtocolAddress);
+      const isSynced = await this._checkIsSyncedByBlockNumber(lowerCasedProtocolAddress);
       if (isSynced) {
         return true;
       }
@@ -197,7 +151,7 @@ export class EtherscanApiService {
       const protocol = await this.protocolRepository.findOne({ where: { protocolAddress: lowerCasedProtocolAddress } });
 
       let savedLastBlockNumber: number;
-      if (protocol) {
+      if (protocol !== null) {
         const protocolId = protocol.id;
         const transactions = await this.transactionRepository.find({
           where: { protocolId },
@@ -209,38 +163,40 @@ export class EtherscanApiService {
         savedLastBlockNumber = 0;
       }
 
-      let page = 1;
-      while (true) {
-        const request: EtherScanTxListRequest = {
-          address: lowerCasedProtocolAddress,
-          startblock: savedLastBlockNumber.toString(),
-          endblock: "99999999",
-          page: page.toString(),
-          offset: "500",
-          sort: "desc",
-          apikey: this.apiKey,
-        };
+      const request: EtherScanTxListRequest = {
+        address: lowerCasedProtocolAddress,
+        startblock: savedLastBlockNumber.toString(),
+        endblock: "99999999",
+        page: "1",
+        offset: "10000",
+        sort: "desc",
+        apikey: this.apiKey,
+      };
 
-        const url = `${this.baseUrl}&address=${request.address}&startblock=${request.startblock}&endblock=${request.endblock}&page=${request.page}&offset=${request.offset}&sort=${request.sort}&apikey=${request.apikey}`;
+      const url = `${this.baseUrl}&address=${request.address}&startblock=${request.startblock}&endblock=${request.endblock}&page=${request.page}&offset=${request.offset}&sort=${request.sort}&apikey=${request.apikey}`;
 
-        const { data } = await firstValueFrom(this.httpService.get<EtherScanResponse>(url).pipe());
+      const { data } = await firstValueFrom(this.httpService.get<EtherScanResponse>(url).pipe());
 
-        if (data.status === "1") {
-          for (const tx of data.result) {
-            await this.createTransaction(tx);
-          }
-
-          /* end loop */
-          if (data.result.length < 500) {
-            break;
-          }
-        } else {
-          throw Error(`Etherscan API Error: status code: ${data.status}`);
+      if (data.status === "1") {
+        for (const tx of data.result) {
+          await this._createTransaction(tx);
         }
-
-        page++;
+      } else {
+        throw Error(`Etherscan API Error: status code: ${data.status}`);
       }
 
+      return true;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  }
+
+  async deleteTables() {
+    try {
+      await this.transactionRepository.delete({});
+      await this.walletRepository.delete({});
+      await this.protocolRepository.delete({});
       return true;
     } catch (error) {
       console.error(error);
