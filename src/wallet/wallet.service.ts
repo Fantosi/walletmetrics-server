9c7366b35c1be7c8e8eb0781817fe82c2026fc33
everGreenGH <skin0011@naver.com>
2023-06-03 14:19:58 +0900

    fix: active, new wallet logic fix

--- src/wallet/wallet.service.ts
+++ src/wallet/wallet.service.ts
@@ -25,7 +25,7 @@ export class WalletService {
     return wallet;
   }
 
-  async getNewWalletsByInterval(
+  async getWalletsByInterval(
     intervalTimestamp: number,
     startTimestamp?: number,
   ): Promise<WalletsByTimestampInterval[]> {
@@ -74,5 +74,55 @@ export class WalletService {
     }
 
     return response;
+  }
+
+  async getNewWalletGrowthRate(intervalTimestamp: number): Promise<number[]> {
+    const endTimestamp = Date.now();
+    const startTimestamp1Day = endTimestamp - intervalTimestamp * 24 * 60 * 60 * 1000; // 1일 전
+    const startTimestamp7Days = endTimestamp - intervalTimestamp * 7 * 24 * 60 * 60 * 1000; // 7일 전
+    const startTimestamp30Days = endTimestamp - intervalTimestamp * 30 * 24 * 60 * 60 * 1000; // 30일 전
+
+    const newWallets1Day = await this.getWalletsByInterval(intervalTimestamp, startTimestamp1Day);
+    const newWallets7Days = await this.getWalletsByInterval(intervalTimestamp, startTimestamp7Days);
+    const newWallets30Days = await this.getWalletsByInterval(intervalTimestamp, startTimestamp30Days);
+
+    const growthRate1Day = this.calculateGrowthRate(newWallets1Day);
+    const growthRate7Days = this.calculateGrowthRate(newWallets7Days);
+    const growthRate30Days = this.calculateGrowthRate(newWallets30Days);
+
+    return [growthRate1Day, growthRate7Days, growthRate30Days];
+  }
+
+  async getCurrentActiveWalletsCount(): Promise<number[]> {
+    const endTimestamp = Date.now();
+    const startTimestampToday = new Date().setHours(0, 0, 0, 0); // 오늘 자정
+    const startTimestampThisWeek = endTimestamp - 7 * 24 * 60 * 60 * 1000; // 이번 주 첫날 자정
+    const startTimestampThisMonth = new Date(new Date().getFullYear(), new Date().getMonth(), 1).getTime(); // 이번 달 1일 자정
+
+    const walletsToday = await this.getWalletsByInterval(startTimestampToday, endTimestamp);
+    const walletsThisWeek = await this.getWalletsByInterval(startTimestampThisWeek, endTimestamp);
+    const walletsThisMonth = await this.getWalletsByInterval(startTimestampThisMonth, endTimestamp);
+
+    const countToday = walletsToday.reduce((count, walletsInterval) => count + walletsInterval.wallets.length, 0);
+    const countThisWeek = walletsThisWeek.reduce((count, walletsInterval) => count + walletsInterval.wallets.length, 0);
+    const countThisMonth = walletsThisMonth.reduce(
+      (count, walletsInterval) => count + walletsInterval.wallets.length,
+      0,
+    );
+
+    return [countToday, countThisWeek, countThisMonth];
+  }
+
+  private calculateGrowthRate(walletsIntervals: WalletsByTimestampInterval[]): number {
+    const firstInterval = walletsIntervals[0];
+    const lastInterval = walletsIntervals[walletsIntervals.length - 1];
+    const initialCount = firstInterval.newWallets.length;
+    const finalCount = lastInterval.newWallets.length;
+
+    if (initialCount === 0) {
+      return 0;
+    }
+
+    return ((finalCount - initialCount) / initialCount) * 100;
   }
 }
