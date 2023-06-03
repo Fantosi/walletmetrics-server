ab62be56a10618a9ceee58f09aa70f7d5e5a8f64
everGreenGH <skin0011@naver.com>
2023-06-03 16:08:03 +0900

    feat: appcontroller에 mocking endpoint 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,24 @@
-import { Controller, Get, Post, Query } from "@nestjs/common";
+import { Body, Controller, Get, Post, Query } from "@nestjs/common";
 import { AppService } from "./app.service";
 import { EtherscanApiService } from "./external-api/etherscan/etherscan-api.service";
+import { GetBriefReq, GetBreifRes } from "./app.dtos";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService, private readonly etherscanApiService: EtherscanApiService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  constructor(private readonly _appService: AppService, private readonly _etherscanApiService: EtherscanApiService) {}
 
   @Post("sync")
   async syncTransactions(@Query("protocolAddress") protocolAddress: string) {
-    return await this.etherscanApiService.syncTransactions(protocolAddress);
+    return await this._etherscanApiService.syncTransactions(protocolAddress);
+  }
+
+  @Get("userbreif")
+  async getUserBrief(@Body() userBreifReq: GetBriefReq): Promise<GetBreifRes> {
+    return await this._appService.getUserBrief(userBreifReq);
+  }
+
+  @Get("txbrief")
+  async getTxBrief(@Body() txBreifReq: GetBriefReq): Promise<GetBreifRes> {
+    return await this._appService.getTxBrief(txBreifReq);
   }
 }
---
+++ src/app.dtos.ts
@@ -0,0 +1,29 @@
+export class GetBriefReq {
+  protocolAddress: string;
+  timeStampInterval: string;
+}
+
+export class NewDataTimestamp {
+  startTimestamp: string; // Date
+  endTimestamp: string; // Date
+  num: number;
+}
+
+export class NewData {
+  address: string;
+  timestamp: string; // Date
+}
+
+export class GetBreifRes {
+  newDatasHistory: {
+    day: number;
+    week: number;
+    month: number;
+  };
+  newDatasChart: NewDataTimestamp[];
+  newDatasList: NewData[]; // currenty 20 (tmp)
+  totalDatasNum: number;
+  activatedDailyDatasNum: number;
+  activatedWeekDatasNum: number;
+  activatedMonthlyDatasNum: number;
+}
